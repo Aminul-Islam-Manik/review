ab19c18fe8791f8d311d8ae7124816d8d6b2a340
Tapushi Nilu <mntapushi123@gmail.com>
2022-10-17 00:04:47 +0600

    added

--- main.js
+++ main.js
@@ -1,72 +1,14 @@
-//local reviews data
-
-const reveiws =[
-    {
-    id: 1,
-    img:"img/p1.jpg",
-    name:"Sara jhon",
-    job:"UX Designer",
-    info:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam facilis quo nihil ducimus, laudantium rerum eligendi reiciendis voluptatibus odit vitae!"
-    },
-    {
-        id: 2,
-        img:"img/p2.jpg",
-        name:"Jhon Doe",
-        job:"WEB Developer",
-        info:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam facilis quo nihil ducimus, laudantium rerum eligendi reiciendis voluptatibus odit vitae!"
-        },
-        {
-        id: 3,
-        img:"img/p3.jpg",
-        name:"Alex",
-        job:"UI Designer",
-        info:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam facilis quo nihil ducimus, laudantium rerum eligendi reiciendis voluptatibus odit vitae!"
-        },
-        {
-        id: 4,
-        img:"img/p4.jpg",
-        name:"Robart",
-        job:"WordPress Expart",
-        info:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam facilis quo nihil ducimus, laudantium rerum eligendi reiciendis voluptatibus odit vitae!"
-        }
-]
-//select items
-const img = document.querySelector('.image')
-console.log(img.src)
-const author = document.querySelector('.author')
-const job = document.querySelector('.job')
-const info = document.querySelector('.info')
-const prevBtn = document.querySelector('.prev-btn')
-const nextBtn = document.querySelector('.next-btn')
-
-//set current item
-let currentItem = 0;
-
-//load initail item
-window.addEventListener('DOMContentLoaded', function (){
-    showReviews(currentItem)
-})
-
-function showReviews(currentItem){
-    const item = reveiws[currentItem];
-    img.src = item.img;
-    author.innerText = item.name;
-    job.innerText = item.job;
-    info.innerText = item.info;
+const clicked = document.getElementsByClassName('extend')
+const modalInfo = document.getElementsByClassName('modal-info')
+for(let i = 0; i < clicked.length; i++){
+    clicked[i].addEventListener('click', function(){
+        // if(modalInfo[i].classList.contains('modal-open')){
+        //     modalInfo[i].classList.remove('modal-open')
+        // }else{
+        //     modalInfo[i].classList.add('modal-open')
+        // }
+        modalInfo[i].classList.toggle('modal-open')
+    })
 }
 
-prevBtn.addEventListener('click', function(){
-    currentItem--;
-    if(currentItem < 0){
-        currentItem = reveiws.length -1;
-    }
-    showReviews(currentItem)
-})
 
-nextBtn.addEventListener('click', function(){
-    currentItem++;
-    if(currentItem > reveiws.length -1){
-        currentItem = 0;
-    }
-    showReviews(currentItem)
-})
